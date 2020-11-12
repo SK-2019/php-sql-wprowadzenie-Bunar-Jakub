4f4bb5529b7a3f7cb36b9d9ea8fbc61430396e67
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-12 22:43:48 +0100

    Update organizacja.php

--- organizacja.php
+++ organizacja.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jakub Bunar</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -12,6 +12,7 @@
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agrygujące</a>
+    <a href="dataczas.php">Data i Czas</a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     
 
