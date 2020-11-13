fff31f9801e8bb41ed3bac3cbc4f6311814274ee
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-13 20:41:27 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn= new mysqli('mysql-jakub-bunar.alwaysdata.net","217227","Jakub200318","jakub-bunar_fajnanauka2');
+$conn= new mysqli("mysql-jakub-bunar.alwaysdata.net","217227","Jakub200318","jakub-bunar_fajnanauka2");
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 
