9764d29734a3d697b29c14de88a43a7ab353fd4a
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-17 10:07:24 +0100

    Add files via upload

---
+++ Send2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo(<$_POST["Firstname"]);
+echo($_POST["Lastname"]);
+echo($_POST["Phone"]);
+echo($_POST["City"]);
+
+?>
+    
+</body>
+</html>
