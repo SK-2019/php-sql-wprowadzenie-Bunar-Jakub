e20de55c89feb29fcbd2a017f394364f4e24c6c6
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-20 14:36:16 +0100

    Update funkcje.php

--- funkcje.php
+++ funkcje.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jakub Bunar</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -13,6 +13,7 @@
     <a href="funkcje.php">Funkcje Agrygujące</a>
     <a href="dataczas.php">Data i Czas</a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
+    <a href="formularz.html">Zakładka testowa</a>
 </div>
 <?php
 
