a6d77fc08bb7e0812f311924e4d977583908d2f5
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-20 15:35:17 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,19 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    require("connect.php");
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    
-    $sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki, data_urodzenia) 
-           VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
-    
-    
-    $conn->query($sql);
-    
-    $conn->close();
-    ?>
+<?php
+  
+    echo("<h2> Imie:".$_POST["imie"]."</h2>");
+echo("<h2> Dział:".$_POST["dzial"]."</h2>");
+echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
+echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
+
+
+require("connect.php");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+         VALUES (null,'Balbina', 4, 86,'1999-05-21')";
+  
+  //obsługa błędów zapisu do bazy
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+  //informacja o ewentualnych błędach
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+  ?>
+
 </body>
 </html>
