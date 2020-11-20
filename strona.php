4c60581ad6ac8913873164bba29bb391d6a1a010
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-20 14:40:54 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,10 +1,37 @@
-<?php
-echo ("strona.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Tekst</title>
+</head>
+<body>
 
-echo(<$_POST["Firstname"]);
-echo($_POST["Lastname"]);
-echo($_POST["Phone"]);
-echo($_POST["City"]);
+<div class=nav>
+    <a href="pracownicy.php">Pracownicy</a>
+    <a href="organizacja.php">Pracownicy i Organizacja</a>
+    <a href="funkcje.php">Funkcje Agrygujące</a>
+    <a href="dataczas.php">Data i Czas</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
+    <a href="formularz.html">Zakładka testowa</a>
+
+
+</div>
+
+
+
+<?php
+
+echo("<h2> Imie:".$_POST["imie"]."</h2>");
+echo("<h2> Nazwisko:".$_POST["nazwisko"]."</h2>");
+echo("<h2> Nr.Tel:".$_POST["nr.tel"]."</h2>");
+echo("<h2> Kod Pocztowy:".$_POST["post"]."</h2>");
+echo("<h2> Miasto:".$_POST["city"]."</h2>");
+
 
 
 ?>
+    
+</body>
+</html>
