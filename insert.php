3a8211a8f75f74117367eadad320becb1fd7e631
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-11-25 23:16:20 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -19,14 +19,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
   
-  $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-         VALUES (null,'Balbina', 4, 86,'1999-05-21')";
+  $sql = ("INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki, data_urodzenia) VALUES (NULL,'".$_POST['imie']."', ".$_POST['dzial'].", ".$_POST['zarobki'].",'".$_POST['data_urodzenia']."')");
+  echo "<li>".$sql;
   
-  //obsługa błędów zapisu do bazy
+  
+  
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
   } else {
-  //informacja o ewentualnych błędach
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
   
