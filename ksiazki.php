b6a0140de31b9dd5ca7bcdcc24bc7913d4083946
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-12-04 12:12:10 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -8,12 +8,11 @@
 </head>
 <body>
 <div class=nav>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub"</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agrygujące</a>
     <a href="dataczas.php">Data i Czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
     <a href="daneDoBazy.php">Dane Do Bazy</a>
     <a href="ksiazki.php">Ksiazki</a>
