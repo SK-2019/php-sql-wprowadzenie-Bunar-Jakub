61e1f26757f2a4bc612a461921bd619826ecaca2
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-12-04 12:46:46 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -42,7 +42,7 @@ $result=$conn->query($sql);
 
 
 $sql = ("SELECT * from biblAutor");
-echo("<h2>".$sql."</h2>");
+echo("<h2>"$sql"</h2>");
 
 $result=$conn->query($sql);
         echo("<table border=1>");
@@ -59,7 +59,7 @@ $result=$conn->query($sql);
 
         
             $sql = ("SELECT * from biblTytul");
-            echo("<h2>".$sql."</h2>");
+            echo("<h2>"$sql"</h2>");
             
             $result=$conn->query($sql);
                     echo("<table border=1>");
