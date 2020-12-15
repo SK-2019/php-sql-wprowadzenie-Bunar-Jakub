641b2c9c2edcf0492ae24f1d6538a137571260fe
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2020-12-15 22:46:36 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -14,7 +14,7 @@
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i Czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub/">Github</a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
     <a href="daneDoBazy.php">Dane Do Bazy</a>
     <a href="ksiazki.php">Ksiazki</a>
@@ -68,7 +68,7 @@ if ($conn->query($sql) === TRUE) {
         }
 
         echo("</table>");
-
+        header("daneDoBazy.php");
 
 
 
