b30a39390bb79e53d0eaecbcf0af3733f3c02e76
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2021-01-29 08:32:37 +0100

    Commit

--- pracownicy/funkcje.php
+++ pracownicy/funkcje.php
@@ -10,7 +10,7 @@
 <div class=nav>
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
-    <a href="funkcje.php">Funkcje Agrygujące</a>
+    <a href="funkcje.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i Czas</a>
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
