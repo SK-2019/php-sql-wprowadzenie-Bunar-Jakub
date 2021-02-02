f9e839f6d1b2f49569eb77cb4d5147104bc87093
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2021-02-02 08:56:46 +0100

    Update

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -13,7 +13,7 @@
     <a class=navlink href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
     <a class=navlink href="../pracownicy/pracownicy.php">Pracownicy</a>
     <a class=navlink href="../pracownicy/organizacja.php">Pracownicy i Organizacja</a>
-    <a class=navlink href="../pracownicy/funkcje.php">Funkcje Agrygujące</a>
+    <a class=navlink href="../pracownicy/funkcje.php">Funkcje Agregujące</a>
     <a class=navlink href="../pracownicy/dataczas.php">Data i Czas</a>
     <a class=navlink href="../pracownicy/formularz.html">Zakładka testowa</a>
     <a class=navlink href="../pracownicy/daneDoBazy.php">Dane Do Bazy</a>
