2ef5495a46d3ae3048fad6087fad633b58b06adf
Bunar-Jakub <jbunar200318@gmail.com>
2021-02-08 21:23:22 +0100

    Update

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -7,21 +7,22 @@
     <link rel="stylesheet" href="../assets/style.css">
 </head> 
 
-
 <body>
+<div class="container">
+<div class="heading">
 <h1>Jakub Bunar nr3</h1>
-<div class=nav>
-    <a class=navlink href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
-    <a class=navlink href="../pracownicy/pracownicy.php">Pracownicy</a>
-    <a class=navlink href="../pracownicy/organizacja.php">Pracownicy i Organizacja</a>
-    <a class=navlink href="../pracownicy/funkcje.php">Funkcje Agregujące</a>
-    <a class=navlink href="../pracownicy/dataczas.php">Data i Czas</a>
-    <a class=navlink href="../pracownicy/formularz.html">Zakładka testowa</a>
-    <a class=navlink href="../pracownicy/daneDoBazy.php">Dane Do Bazy</a>
-    <a class=navlink href="ksiazki.php">Ksiazki</a>
-    
-
 </div>
+<div class=nav>
+    <a class="navlink" href="https://github.com/SK-2019/php-sql-wprowadzenie-Bunar-Jakub">Github</a>
+    <a class="navlink" href="../pracownicy/pracownicy.php">Pracownicy</a>
+    <a class="navlink" href="../pracownicy/organizacja.php">Pracownicy i Organizacja</a>
+    <a class="navlink" href="../pracownicy/funkcje.php">Funkcje Agregujące</a>
+    <a class="navlink" href="../pracownicy/dataczas.php">Data i Czas</a>
+    <a class="navlink" href="../pracownicy/formularz.html">Zakładka testowa</a>
+    <a class="navlink" href="../pracownicy/daneDoBazy.php">Dane Do Bazy</a>
+    <a class="navlink" href="ksiazki.php">Ksiazki</a>
+</div>
+<div class="php">
 <?php
 
 require("../assets/connect.php");
@@ -76,3 +77,5 @@ $result=$conn->query($sql);
                         }
                     echo("</table>");
 ?>
+    </div>
+</div>
