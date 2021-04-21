0ed6264a37cac64c28659e3c247d4650f975ee85
Bunar-Jakub <73523115+Bunar-Jakub@users.noreply.github.com>
2021-04-21 21:50:22 +0200

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,21 +1,18 @@
 <?php
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname == 'localhost') {
+     require_once ("config.php");
+ }
 
 $servername = $_SERVER['servername'];
 $username = $_SERVER['username'];
 $password = $_SERVER['password'];
-$dbname = $_SERVER['dbname'];
+$dbname = $_SERVER['dbname']; 
 
-$hostname = $_SERVER['HTTP_HOST']; 
-// echo("<li> hostname : ".$hostname);
-// echo("<li> SERVER passsword: ".$_SERVER['password']);
 
-    if ($hostname == 'bunar-jakub.herokuapp.com') {
-        require_once ("config.php");
-    }
+$conn=new mysqli($servername, $username, $password, $dbname);
+        if($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-$conn= new mysqli($servername , $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("connection failed: ".mysqli_connect_error());
-
-    }
 ?>
